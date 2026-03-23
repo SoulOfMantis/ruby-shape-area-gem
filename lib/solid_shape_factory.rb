00d43c9735f181dd7fd2be56686051eb3a92298c
Andrey <timo@sfedu.ru>
2026-03-23 14:00:39 +0300

    finished SolidShapeFactory

--- lib/solid_shape_factory.rb
+++ lib/solid_shape_factory.rb
@@ -1,4 +1,5 @@
-class Solid_shape_factory
+require_relative 'solid_shape.rb'
+class SolidShapeFactory
   attr_accessor :solid_shape
   def initialize(solid_shape)
     change_shape solid_shape
@@ -8,11 +9,19 @@ class Solid_shape_factory
   end
   def create(args)
     case @solid_shape
-    when :right_prism then Right_prism.new args[:polygon_base], args[:height]
-    #In case of regular hexagonal prism, base may either be a regular hexagon or an integer (regular hexagon's side)
-    when :regular_hexagonal_prism then Regular_hexagonal_prism.new args[:base], args[:height]
-    #TODO: implement all solid shapes and change the error to the ArgumentError
-    else raise NotImplementedError
+    when :right_prism then RightPrism.new args[:polygon_base], args[:height]
+    when :regular_hexagonal_prism then RegularHexagonalPrism.new args[:base], args[:height]
+    when :sphere then Sphere.new args[:radius]
+    when :right_cuboid then RightCuboid.new args[:width], args[:length], args[:height]
+    when :cube then Cube.new args[:side]
+    when :cone then Cone.new args[:base_radius], args[:height]
+    when :pyramid then Pyramid.new args[:polygon_base], args[:height]
+    when :elliptical_cylinder then EllipticalCylinder.new args[:semi_major], args[:semi_minor], args[:height]
+    when :trapezoidal_prism then TrapezoidalPrism.new args[:base1], args[:base2], args[:side1], args[:side2],
+                                                      args[:base_height], args[:prism_height]
+    when :right_rhombic_prism then RightRhombicPrism.new args[:diagonal1], args[:diagonal2], args[:height]
+    when :regular_tetrahedron then RegularTetrahedron.new args[:side]
+    else raise ArgumentError
     end
   end
 end
